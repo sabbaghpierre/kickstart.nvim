afd17d5925e787ad44c04f2874b308c3f9863cda
Pierre Sabbagh <sabbaghpierre@proton.me>
2025-09-14 11:19:25 +0300

    refactor: remove duplicate clipboard option

--- init.lua
+++ init.lua
@@ -181,9 +181,6 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 -- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
--- Bind copy to system clipboard
-vim.api.nvim_set_option('clipboard', 'unnamed')
-
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
