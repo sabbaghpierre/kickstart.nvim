c8940b4878cace95039072e4d3c4b40759aae4d4
Pierre Sabbagh <sabbaghpierre@proton.me>
2026-03-08 10:21:12 +0200

    refactor: git signs config

--- init.lua
+++ init.lua
@@ -283,18 +283,7 @@ require('lazy').setup({
   -- options to `gitsigns.nvim`.
   --
   -- See `:help gitsigns` to understand what the configuration keys do
-  { -- Adds git related signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
-  },
+  -- Gitsigns moved to lua/kickstart/plugins/gitsigns.lua
 
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
