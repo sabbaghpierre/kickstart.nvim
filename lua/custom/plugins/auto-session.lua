2d0409d80da84df4f66c236b08889f0fa4fdd4b9
Pierre Sabbagh <pierresabbagh47@gmail.com>
2026-03-23 21:59:57 +0200

    feat: add session options to autosession plugin

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -1,7 +1,19 @@
+vim.o.sessionoptions = 'blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions'
+
 return {
   'rmagatti/auto-session',
   lazy = false,
   opts = {
     suppressed_dirs = { '~/', '~/Downloads', '/' },
+    pre_save_cmds = {
+      function()
+        -- Close oil buffers before saving session to avoid restoring broken oil:// URIs
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+          if vim.bo[buf].filetype == 'oil' then
+            vim.api.nvim_buf_delete(buf, { force = true })
+          end
+        end
+      end,
+    },
   },
 }
