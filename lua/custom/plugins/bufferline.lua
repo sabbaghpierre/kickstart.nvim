344980274f2ba043475f63dcf88fd46683323a9e
Pierre Sabbagh <pierresabbagh47@gmail.com>
2026-03-23 21:59:35 +0200

    refactor: initialize bufferline plugin keys using keys parameter instead of vim.keymap.set

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -3,23 +3,29 @@ return {
   version = '*',
   event = 'VeryLazy',
   dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function()
-    require('bufferline').setup {
-      options = {
-        -- ... your other configuration options (if any) ...
-      },
-    }
-
-    -- Navigation: Use BufferLine's own commands for smooth cycling
-    vim.keymap.set('n', '<Leader>bn', '<cmd>BufferLineCycleNext<CR>', { desc = 'Next Buffer' })
-    vim.keymap.set('n', '<Leader>bp', '<cmd>BufferLineCyclePrev<CR>', { desc = 'Previous Buffer' })
-
-    -- MAGIC: Interactive buffer picker - highly recommended!
-    vim.keymap.set('n', '<Leader>bb', '<cmd>BufferLinePick<CR>', { desc = 'Pick Buffer' })
-    -- Close current buffer
-    vim.keymap.set('n', '<Leader>bd', '<cmd>bdelete<CR>', { desc = 'Delete Buffer' })
-
-    -- NEW: Close all buffers except current one
-    vim.keymap.set('n', '<Leader>bo', '<cmd>BufferLineCloseOthers<CR>', { desc = 'Close Other Buffers' })
-  end,
+  opts = {},
+  keys = {
+    { '<Leader>bn', '<cmd>BufferLineCycleNext<CR>', desc = 'Next Buffer' },
+    { '<Leader>bp', '<cmd>BufferLineCyclePrev<CR>', desc = 'Previous Buffer' },
+    { '<Leader>bb', '<cmd>BufferLinePick<CR>', desc = 'Pick Buffer' },
+    {
+      '<Leader>bd',
+      function()
+        local current = vim.api.nvim_get_current_buf()
+        local next_buf = nil
+        for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+          if buf ~= current and vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted and vim.bo[buf].buftype ~= 'terminal' then
+            next_buf = buf
+            break
+          end
+        end
+        if next_buf then
+          vim.api.nvim_set_current_buf(next_buf)
+        end
+        vim.cmd('bdelete ' .. current)
+      end,
+      desc = 'Delete Buffer',
+    },
+    { '<Leader>bo', '<cmd>BufferLineCloseOthers<CR>', desc = 'Close Other Buffers' },
+  },
 }
