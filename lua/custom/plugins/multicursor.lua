42deeae750ca2a24c3986d93d8835696354f42d7
Pierre Sabbagh <pierresabbagh47@gmail.com>
2026-03-23 22:06:05 +0200

    feat: add multicursor plugin

---
+++ lua/custom/plugins/multicursor.lua
@@ -0,0 +1,41 @@
+return {
+  -- Keyboard-based multicursor (works everywhere including Warp)
+  {
+    'mg979/vim-visual-multi',
+    branch = 'master',
+  },
+
+  -- Mouse-based multicursor: Ctrl+Click (requires iTerm2, WezTerm, kitty)
+  {
+    'jake-stewart/multicursor.nvim',
+    branch = '1.0',
+    config = function()
+      local mc = require 'multicursor-nvim'
+      mc.setup()
+
+      local set = vim.keymap.set
+
+      set('n', '<c-leftmouse>', mc.handleMouse)
+      set('n', '<c-leftdrag>', mc.handleMouseDrag)
+      set('n', '<c-leftrelease>', mc.handleMouseRelease)
+
+      mc.addKeymapLayer(function(layerSet)
+        layerSet('n', '<esc>', function()
+          if not mc.cursorsEnabled() then
+            mc.enableCursors()
+          else
+            mc.clearCursors()
+          end
+        end)
+      end)
+
+      local hl = vim.api.nvim_set_hl
+      hl(0, 'MultiCursorCursor', { reverse = true })
+      hl(0, 'MultiCursorVisual', { link = 'Visual' })
+      hl(0, 'MultiCursorSign', { link = 'SignColumn' })
+      hl(0, 'MultiCursorDisabledCursor', { reverse = true })
+      hl(0, 'MultiCursorDisabledVisual', { link = 'Visual' })
+      hl(0, 'MultiCursorDisabledSign', { link = 'SignColumn' })
+    end,
+  },
+}
