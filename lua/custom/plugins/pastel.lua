6d32e9cfe909b8dfb97c397d0bbacee9a19d6a72
Pierre Sabbagh <pierresabbagh47@gmail.com>
2026-04-08 19:30:55 +0300

    feat: add new pastel colorscheme

---
+++ lua/custom/plugins/pastel.lua
@@ -0,0 +1,7 @@
+return {
+  'ankushbhagats/pastel.nvim',
+  lazy = false, -- disable lazy loading
+  priority = 1000, -- load immediately at startup
+  opts = {}, -- your configuration comes here
+  config = true, -- call setup function with provided opts
+}
